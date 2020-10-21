1468eac26cca42a7edefaa7f6f6261eb270c9860
Giovanni Rodriguez G <34606945+grg8881m@users.noreply.github.com>
2020-10-21 17:18:11 -0500

    Add files via upload

---
+++ IteradoresFor/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+        printf("Iterador For\n");
+
+        int upperLim, bottomLim;
+
+        printf("Imprimir en orden descendiente \n");
+
+        printf("Ingresar limite superior: \n");
+        scanf("%i", &upperLim);
+
+        printf("Ingresar limite inferior: \n");
+        scanf("%i", &bottomLim);
+
+        for(int i= upperLim; i >=bottomLim; i--)
+        {
+            printf("Numero: %i \n", i);
+        }
+
+    return 0;
+}
